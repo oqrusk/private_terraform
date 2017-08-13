abd2c7dbae66f9c8fadb4035ab94802dcfc940fb
oqrusk <y.okura@timers-inc.com>
2017-08-13 13:20:54 +0900

    enable web cluster

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ variable "server_port" {
   default = 8080
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-40d28157"
+resource "aws_launch_configuration" "example" {
+  image_id      = "ami-40d28157"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  security_groups = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -18,8 +18,25 @@ resource "aws_instance" "example" {
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 
-  tags {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+  load_balancers    = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
+
+  max_size = 5
+  min_size = 2
+
+  tag {
+    key = "Name"
+    propagate_at_launch = false
+    value = "terraform-asg-sample"
   }
 }
 
@@ -38,6 +55,57 @@ resource "aws_security_group" "instance" {
   }
 }
 
-output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
+
+resource "aws_elb" "example" {
+  name = "terraform-asg-example"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups = ["${aws_security_group.elb.id}"]
+
+  listener {
+    instance_port = "${var.server_port}"
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+    timeout = 3
+    unhealthy_threshold = 2
+  }
+
+}
+
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+
+  ingress {
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+
+data "aws_availability_zones" "all" {
+
+}
+
+output "elb_dns_name" {
+  value = "${aws_elb.example.dns_name}"
 }
